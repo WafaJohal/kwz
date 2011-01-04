67a95e83b4f1f9239a86c4b63854c45e2f6005da
Wafa Benkaouar <wafa.benkaouar@gmail.com>
2011-01-04 19:15:16 +0100

    v0.0 First version of the interface

---
+++ Interface.java
@@ -0,0 +1,108 @@
+
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.Panel;
+import java.awt.GridBagLayout;
+import java.awt.Color;
+public class Interface {
+
+	private JFrame jFrame = null;  //  @jve:decl-index=0:visual-constraint="108,94"
+	private JPanel jContentPane = null;
+	private JMenuBar jJMenuBar = null;
+	private JMenu fileMenu = null;
+	private JMenu editMenu = null;
+	private JMenu helpMenu = null;
+	/**
+	 * This method initializes jFrame	
+	 * 	
+	 * @return javax.swing.JFrame	
+	 */
+	private JFrame getJFrame() {
+		if (jFrame == null) {
+			jFrame = new JFrame();
+			jFrame.setSize(new Dimension(300, 200));
+			jFrame.setTitle("Image Software");
+			jFrame.setBackground(Color.black);
+			jFrame.setForeground(Color.orange);
+			jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			jFrame.setVisible(true);
+			jFrame.setJMenuBar(getJJMenuBar());
+			jFrame.setContentPane(getJContentPane());
+		}
+		return jFrame;
+	}
+
+	/**
+	 * This method initializes jContentPane	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getJContentPane() {
+		if (jContentPane == null) {
+			jContentPane = new JPanel();
+			jContentPane.setLayout(new BorderLayout());
+		}
+		return jContentPane;
+	}
+
+	/**
+	 * This method initializes jJMenuBar	
+	 * 	
+	 * @return javax.swing.JMenuBar	
+	 */
+	private JMenuBar getJJMenuBar() {
+		if (jJMenuBar == null) {
+			jJMenuBar = new JMenuBar();
+			jJMenuBar.setPreferredSize(new Dimension(0, 20));
+			jJMenuBar.add(getFileMenu());
+			jJMenuBar.add(getEditMenu());
+			jJMenuBar.add(getHelpMenu());
+		}
+		return jJMenuBar;
+	}
+
+	/**
+	 * This method initializes fileMenu	
+	 * 	
+	 * @return javax.swing.JMenu	
+	 */
+	private JMenu getFileMenu() {
+		if (fileMenu == null) {
+			fileMenu = new JMenu();
+			fileMenu.setText("File");
+		}
+		return fileMenu;
+	}
+
+	/**
+	 * This method initializes editMenu	
+	 * 	
+	 * @return javax.swing.JMenu	
+	 */
+	private JMenu getEditMenu() {
+		if (editMenu == null) {
+			editMenu = new JMenu();
+			editMenu.setText("Edit");
+		}
+		return editMenu;
+	}
+
+	/**
+	 * This method initializes helpMenu	
+	 * 	
+	 * @return javax.swing.JMenu	
+	 */
+	private JMenu getHelpMenu() {
+		if (helpMenu == null) {
+			helpMenu = new JMenu();
+			helpMenu.setText("Help");
+		}
+		return helpMenu;
+	}
+
+}
